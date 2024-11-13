43293461d6497e5d4bb52662606cbff546af75a8
sonechka1234 <sofizhbanova@gmail.com>
2024-11-13 18:35:48 +0300

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+def f():
+    arr = []
+    arr.append('1')
+    print(arr)
+f()
+class Car():
+    def ride(self):
+        print('gb')
+    def ride1(self):
+        print('1')
+car = Car()
+car.ride()
