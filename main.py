e5eb29d66195bd53143e1c30e6e1884c412d759e
sonechka1234 <sofizhbanova@gmail.com>
2024-11-15 19:23:40 +0300

    Добавлен базвовый  класс User

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 
-
-def f():
-    arr = []
-    arr.append('1')
-    print(arr)
-f()
-class Car():
-    def ride(self):
-        print('gb')
-    def ride1(self):
-        print('1')
-car = Car()
-car.ride()
+# from abc import FBC
+# def f():
+#     arr = []
+#     arr.append('1')
+#     print(arr)
+# f()
+# class ICar(ABC):
+    
+# class Car():
+#     def ride(self):
+#         print('gb')
+#         self.ride1()
+#     def ride1(self):
+#         print('1')
+# car = Car()
+# car.ride() оаерпаегпагнорагнеп6гноегноеп
